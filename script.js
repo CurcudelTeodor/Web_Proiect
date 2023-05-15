e2623131ab2c0e270b25d26fb3abe50798c933cf
Alexandru Hrebenciuc <alex.hrebenciuc@yahoo.com>
2023-05-15 23:18:03 +0300

    Am golit main-ul pentru a adauga fiecare ce a facut

--- script.js
+++
@@ -1,6 +0,0 @@
-let menu = document.querySelector('#icon-menu');
-let navbar=document.querySelector('.navbar');
-menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navbar.classList.toggle('open');
-}
