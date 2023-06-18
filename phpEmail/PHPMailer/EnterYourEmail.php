4c925abbe9af21587642e42e5dbfde012e86f8d5
iustinnn <98700219+iustinnn@users.noreply.github.com>
2023-06-18 20:39:07 +0300

    Add files via upload

---
+++ phpEmail/PHPMailer/EnterYourEmail.php
@@ -0,0 +1,33 @@
+<?php
+include ("phpEmail/email.php");
+class EnterYourEmail{
+    public function verify_email($data){
+$email = addslashes($data['email']);
+$query = "select * from useri where email = '$email' limit 1";
+$DB = new Database();
+$EM = new Email();
+$result = $DB->read($query);
+if ($result) {
+
+    $row = $result[0];
+    $_SESSION['emailRecovery'] = $email;
+    
+$EM->initialize($email); 
+$EM->send($email);
+
+        $DB->save($query);
+  header("Location: EnterVerifCode/enterverifcode.html");
+    exit;
+ echo "da";
+} else {
+    header("Location: EnterYourEmail.html");
+    exit;
+}
+    }
+}
+
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $enter = new EnterYourEmail();
+    $enter->verify_email($_POST);
+}
